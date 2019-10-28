2266c34221a63655308e5bda510bff50e884e72d
RakeshGontla <rakesh.gontla@ideabytes.com>
2019-10-28 12:11:07 +0530

    add new.js code  to dev branch

---
+++ src/Home/sample/newfile.js
@@ -0,0 +1,54 @@
+/**
+ * BF Circle Project(Subscriber)
+ * Start Date:- july 02,2019
+ * Modified Date:-
+ * Created by:- Anand Namoju
+ * Modified by:- Anand Namoju, Ravi kiran
+ * Last modified by:- 19 Aug, 2019
+ * Todo:- 
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { 
+    Text, 
+    View, 
+    Alert, 
+    StatusBar, 
+    StyleSheet, 
+    TouchableOpacity, 
+    Platform, 
+    Dimensions,
+    SafeAreaView,
+    ScrollView 
+ } from 'react-native';
+
+
+
+const{height, width} = Dimensions.get('window');
+
+export default class sample extends Component {
+    static navigationOptions = {
+        header: null
+    };
+
+    constructor(props){
+        super(props);
+        this.state = {
+           
+        }
+    }
+
+  
+    
+   
+
+  render() {
+    return (
+        <Text style={{ color: 'red', fontSize: 16, fontWeight: 'bold' }}>Beauty & {'\n'}Fitness Circle123</Text>
+
+    );
+  }
+}
+
