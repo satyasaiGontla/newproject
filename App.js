1e09ca5818aaa6b58d90d4415b5acca0c49e66f6
RakeshGontla <rakesh.gontla@ideabytes.com>
2019-10-31 14:17:14 +0530

    add dev1.js file to dev1

--- App.js
+++ App.js
@@ -1,114 +1,43 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
 
+//sample Example to understand React Native State//
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
+//To import the react.
+ 
+import { Text, View } from 'react-native';
+//To import all the required elements we are going to use.
+ 
+export default class App extends React.Component {
+ 
+  constructor(props) {
+    super(props);
+    // State sould be defined in constructor of class. 
+    this.state = {valx:0, valy:100000000};
+    // you can define N number of key value paires like JSON.
+    setInterval(() => {
+      this.setState({valx : this.state.valx+1});
+      this.setState({valy : this.state.valy-1});
+    }, 1000);
+    // Simple interval fundtion which will run in every second.
+    // It will increase valx and decrese valy
+  }
+ 
+  render() {
+    return (
+      <View>
+ 
+        <Text>{"Hello I am valx. I am increasing "+this.state.valx}</Text>
+        {/*// Here we have bind the state of valx with the above text.
+        // So whenever the setState called to change value of the valx
+        // Text Component will be re-render every time.
+        // valx will change in 1 Sec in our example.*/}
+ 
+        <Text>{"Hello I am valy. I am decreasing "+this.state.valy}</Text>
+        {/*// Here we have bind the state of valy with the above text.
+        // So whenever the setState called to change value of the valy
+        // Text Component will be re-render every time.
+        // valy will change in 1 Sec in our example.*/}
+ 
+      </View>
+    );
+  }
+}
